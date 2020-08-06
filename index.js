bc78cb21a4d5873a114e4060e97ce2f9e9519c00
Aiannn <soktoevayan@gmail.com>
2020-08-06 18:28:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users",  {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email  
+        })
+    })
+    .then(response => {return response.json()})
+    .then( (object)=> {document.body.innerHTML = object["id"]})
+    .catch(function(error) {
+        alert("Bad things mfaka")
+        document.body.innerHTML = error.message
+    })
+}
